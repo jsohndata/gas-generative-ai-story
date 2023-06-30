aae4837a2bcb4e660829d437972a53ae1288f59d
jSohnData <jihos@me.com>
2023-06-30 01:13:09 -0400

    ADDED cyber male

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -9,23 +9,28 @@ const videoFile1 = imageUri+'/motion-cyber-girl.mp4';
 const imageFile2 = imageUri+'/motion-cyberpunk-city.jpg';
 const videoFile2 = imageUri+'/motion-cyberpunk-city.mp4';
 
-const Page = () => {
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+const imageFile3 = imageUri+'/motion-cyber-male.png';
+const videoFile3 = imageUri+'/motion-cyberpunk-city.mp4';
 
-  const handleClick = () => {
-    setIsVideoPlaying(true);
+const Page = () => {
+  const [isVideoPlaying1, setIsVideoPlaying1] = useState(false);
+  const [isVideoPlaying2, setIsVideoPlaying2] = useState(false);
+  const [isVideoPlaying3, setIsVideoPlaying3] = useState(false);
+
+  const toggleView = (videoPlayingId: any) => {
+    videoPlayingId(true);
   };
 
   return (
     <AppLayout>
       <Container>
         <Row>
-          <Col>
-            {!isVideoPlaying ? (
+          <Col md={6}>
+            {!isVideoPlaying1 ? (
               <Image 
                 src={imageFile1} 
                 alt="Click Me" 
-                onClick={handleClick}
+                onClick={ () => toggleView(setIsVideoPlaying1)}
                 style={{ width: '100%', }} />
             ) : (
               <video         
@@ -43,12 +48,13 @@ const Page = () => {
           </Col>
 
 
-          <Col>
-            {!isVideoPlaying ? (
+          <Col md={6}>
+            {!isVideoPlaying2 ? (
               <Image 
+                fluid
                 src={imageFile2} 
                 alt="Click Me" 
-                onClick={handleClick}
+                onClick={ () => toggleView(setIsVideoPlaying2)}
                 style={{ width: '100%', }} />
             ) : (
               <video         
@@ -61,6 +67,29 @@ const Page = () => {
                   margin: '0',
                 }}>
                 <source src={videoFile2} type="video/mp4" />
+              </video>
+            )}
+          </Col>
+
+          <Col md={6}>
+            {!isVideoPlaying3 ? (
+              <Image 
+                fluid
+                src={imageFile2} 
+                alt="Click Me" 
+                onClick={ () => toggleView(setIsVideoPlaying2)}
+                style={{ width: '100%', }} />
+            ) : (
+              <video         
+                autoPlay
+                controls
+                style={{ 
+                  width: '100%', 
+                  border: 'none',
+                  padding: '0',
+                  margin: '0',
+                }}>
+                <source src={videoFile3} type="video/mp4" />
               </video>
             )}
           </Col>
