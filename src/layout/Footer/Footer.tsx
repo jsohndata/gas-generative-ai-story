84c034b1ca7067423c25650ce27459a766c947d7
Jiho Sohn <jiho@HummingBird.attlocal.net>
2024-06-20 02:17:12 -0400

    UPDATE footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -5,8 +5,15 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col> 
-            <p>Footer</p>
+        <Col>
+            <p className="text-center">
+            <a href="https://github.com/jsohndata/bocacode-c10-final-project-portfolio" target="_blank" rel="noreferrer" className="text-white"><Github color="white" size={40} />
+            <br />
+            See Code in GitHub</a></p>
+            <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=19"
+              rel="noreferrer"
+              alt="Click on it and press control-shift."
+              title="Click on it and press control-shift.">π</a></p>
           </Col>
         </Row>
       </Container>
