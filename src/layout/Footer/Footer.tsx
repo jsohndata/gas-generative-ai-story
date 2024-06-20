bab94933223a977056c9e0dc167f362d23269e1b
Jiho Sohn <jiho@HummingBird.local>
2024-06-20 09:11:36 -0400

    ADDED  styles for mozart's ghost

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -8,6 +8,7 @@ const Footer = () => {
         <Col>
             <p className="text-center">
               <a href="https://github.com/jsohndata/bocacode-c10-final-project-portfolio" target="_blank" rel="noreferrer" className="text-white">See Code in GitHub</a></p>
+              
               <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=19"
                 rel="noreferrer"
                 title="Click on it and press control-shift.">π</a></p>
