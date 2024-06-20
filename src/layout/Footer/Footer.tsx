0b20d0e51828257a52babb012140685915851ffa
Jiho Sohn <jiho@HummingBird.local>
2024-06-20 08:59:53 -0400

    ADDED Mozarts ghost

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -12,7 +12,6 @@ const Footer = () => {
             See Code in GitHub</a></p>
             <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=19"
               rel="noreferrer"
-              alt="Click on it and press control-shift."
               title="Click on it and press control-shift.">π</a></p>
           </Col>
         </Row>
