f29d7a0d819152d473a07686bf9969fe4ce38ad2
Jiho Sohn <jsohndata@gmail.com>
2024-07-04 18:41:40 -0400

    UPDATE morzart ghost id

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <p className="text-center">
               <a href="https://github.com/jsohndata/gas-generative-ai-story" target="_blank" rel="noreferrer" className="text-white">See Code in GitHub</a></p>
               
-              <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=19"
+              <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=gasai"
                 rel="noreferrer"
                 title="Click on it and press control-shift.">π</a></p>
           </Col>
